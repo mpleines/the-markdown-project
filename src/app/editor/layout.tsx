3eb72f664782bb22c36b30484e05008342bfa0c8
maik pleines <mpleines@auel.de>
2024-04-03 12:01:32 +0200

    add user avatar dropdown

--- src/app/editor/layout.tsx
+++ src/app/editor/layout.tsx
@@ -1,28 +1,22 @@
-import { Metadata } from 'next'
-import Header from '@/components/Header'
-import Sidebar from '@/components/Sidebar'
- 
+import { Metadata } from 'next';
+import Header from '@/components/Header';
+import Sidebar from '@/components/Sidebar';
+
 export const metadata: Metadata = {
   title: 'Getting Started',
   description: 'Get started using The Markdown Project',
-}
+};
 
-export default function EditorLayout({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
-    return (
-      <section className="h-screen">
-        <Header/>
-        <div className="h-full grid grid-cols-6 pt-2 border-t">
-          <div className="h-full col-span-1 hidden lg:block">
-            <Sidebar/>
-          </div>
-          <div className="col-span-4">
-            {children}
-          </div>
+export default function EditorLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <section className="h-screen">
+      <Header />
+      <div className="h-full grid grid-cols-6 pt-2 border-t">
+        <div className="h-full col-span-1 hidden lg:block">
+          <Sidebar />
         </div>
-      </section>
-    )
-  }
+        <div className="col-span-4">{children}</div>
+      </div>
+    </section>
+  );
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,31 @@
 'use client';
 
-import { FunctionComponent } from 'react';
-import { Button } from './ui/button';
-import { signOut } from '@/app/login/actions';
-import { File } from '@geist-ui/icons';
+import { FunctionComponent, useEffect, useState } from 'react';
+import { File, LogOut } from '@geist-ui/icons';
 import Link from 'next/link';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { signOut } from '@/app/login/actions';
+import { createClient } from '@/utils/supabase/client';
+import { User } from '@supabase/supabase-js';
 
 interface HeaderProps {}
 
 const Header: FunctionComponent<HeaderProps> = () => {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const supabase = createClient();
+    supabase.auth.getUser().then((response) => setUser(response.data.user));
+  }, []);
+
   return (
     <nav className="top-0 h-16 w-full flex justify-between items-center py-2 px-4">
       <div>
@@ -18,9 +35,22 @@ const Header: FunctionComponent<HeaderProps> = () => {
         </Link>
       </div>
       <div>
-        <Button onClick={() => signOut()} variant="secondary">
-          Logout
-        </Button>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Avatar>
+              <AvatarImage src="/avatar_placeholer.png" alt="avatar fallback" />
+              <AvatarFallback>MP</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>{user?.email ?? 'My Account'}</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem onClick={() => signOut()} className="flex gap-2 ">
+              <LogOut />
+              <div>Logout</div>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </nav>
   );
