cf11da5b36f3c6e3203a4dcf1bb981b3a56cac32
maik pleines <maik.pleines@gmail.com>
2024-05-22 20:28:27 +0200

    Add block formatting options and controls

--- src/components/Block.tsx
+++ src/components/Block.tsx
@@ -1,10 +1,39 @@
 import { marked } from 'marked';
-import { KeyboardEvent, useEffect, useRef, useState } from 'react';
+import React, { KeyboardEvent, useEffect, useRef, useState } from 'react';
 import ContentEditable, { ContentEditableEvent } from 'react-contenteditable';
 import { useEditorStore } from './Editor';
 import { createClient } from '@/utils/supabase/client';
 import { useDebouncedCallback } from '@/hooks/useDebouncedCallback';
 import { useNotesStore } from '@/stores/notesStore';
+import {
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+  DropdownMenuGroup,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+} from './ui/dropdown-menu';
+
+const markdownCharMap = {
+  h1: '# ',
+  h2: '## ',
+  h3: '### ',
+  h4: '#### ',
+  h5: '##### ',
+  h6: '###### ',
+};
+
+type TagName = keyof typeof markdownCharMap;
+
+const FORMATTING_OPTIONS = [
+  { label: 'Heading 1', value: 'h1' },
+  { label: 'Heading 2', value: 'h2' },
+  { label: 'Heading 3', value: 'h3' },
+  { label: 'Heading 4', value: 'h4' },
+  { label: 'Heading 5', value: 'h5' },
+  { label: 'Heading 6', value: 'h6' },
+];
 
 export type BlockType = {
   id: string;
@@ -35,6 +64,7 @@ export const setCaretToEnd = (element: HTMLElement) => {
 const Block: React.FunctionComponent<BlockProps> = ({ block, addBlock, removeBlock }) => {
   const supabase = createClient();
   const [tagName, setTagName] = useState(block.tag ?? '');
+  const [showBlockControls, setShowBlockControls] = useState(false);
   const ref = useRef<HTMLElement>();
   const plainText = useRef<string>(block.content ?? '');
   const updateBlock = useEditorStore((state) => state.updateBlock);
@@ -52,7 +82,7 @@ const Block: React.FunctionComponent<BlockProps> = ({ block, addBlock, removeBlo
   };
 
   const replaceSpecialChars = (str: string) => {
-    return str.replace(/&nbsp;/g, ' ');
+    return str.replace(/&nbsp;(?=\S)/g, ' ');
   };
 
   const saveToDatabaseDebounced = useDebouncedCallback(async () => {
@@ -65,6 +95,13 @@ const Block: React.FunctionComponent<BlockProps> = ({ block, addBlock, removeBlo
   const handleInputChange = (event: ContentEditableEvent) => {
     const targetValue = event.target.value;
     plainText.current = targetValue;
+
+    const lastChar = targetValue[targetValue.length - 1];
+    if (lastChar === '/') {
+      setShowBlockControls(true);
+    } else {
+      setShowBlockControls(false);
+    }
 
     const trimmedString = replaceSpecialChars(targetValue).trim();
     const tag = getTagName(trimmedString);
@@ -94,6 +131,18 @@ const Block: React.FunctionComponent<BlockProps> = ({ block, addBlock, removeBlo
     }
   };
 
+  const handleMenuItemClick = (newTagName: TagName) => {
+    let newText = plainText.current.replace('/', markdownCharMap[newTagName] + ' ');
+    if (!newText.startsWith(markdownCharMap[newTagName] + ' ')) {
+      newText = newText.replace(markdownCharMap[newTagName], markdownCharMap[newTagName] + ' ');
+    }
+
+    setTagName(newTagName);
+    plainText.current = newText;
+
+    setShowBlockControls(false);
+  };
+
   useEffect(() => {
     if (ref.current) {
       ref.current.focus();
@@ -101,16 +150,41 @@ const Block: React.FunctionComponent<BlockProps> = ({ block, addBlock, removeBlo
     }
   }, [tagName]);
 
+  useEffect(() => {
+    if (showBlockControls && ref.current) {
+      ref.current.focus();
+    }
+  }, [showBlockControls]);
+
   return (
-    <ContentEditable
-      style={{ padding: '2px' }}
-      innerRef={ref as any}
-      key={`block-${tagName}-${block.id}`}
-      html={plainText.current}
-      tagName={tagName}
-      onChange={handleInputChange}
-      onKeyDown={(event) => handleKeyDown(event as any)}
-    />
+    <div style={{ position: 'relative' }}>
+      <ContentEditable
+        style={{ padding: '2px' }}
+        innerRef={ref as any}
+        key={`block-${tagName}-${block.id}`}
+        html={plainText.current}
+        tagName={tagName}
+        onChange={handleInputChange}
+        onKeyDown={(event) => handleKeyDown(event as any)}
+      />
+
+      {showBlockControls && (
+        <DropdownMenu defaultOpen>
+          <DropdownMenuTrigger />
+          <DropdownMenuContent side="bottom" alignOffset={-10}>
+            <DropdownMenuLabel>Formatting</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              {FORMATTING_OPTIONS.map(({ label, value }) => (
+                <DropdownMenuItem key={value} onClick={() => handleMenuItemClick(value)}>
+                  {label}
+                </DropdownMenuItem>
+              ))}
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )}
+    </div>
   );
 };
 
